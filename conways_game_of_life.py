43d03c45ee04bf613f2eff1b2bc8e564aec330e6
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-03 19:22:38 +0300

    conways game of life design problem statement

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -1,28 +1,3 @@
-# The Game of Life, also known simply as Life, is a cellular automaton devised by
-# the British mathematician John Horton Conway in 1970
-
-# It is a zero-player game, meaning that its evolution is determined by its initial state,
-# requiring no further input. One interacts with the Game of Life by creating an
-# initial configuration and observing how it evolves. It is Turing complete and
-# can simulate a universal constructor or any other Turing machine.
-
-# The universe of the Game of Life is an infinite, two-dimensional orthogonal
-# grid of square cells, each of which is in one of two possible states, alive or
-# dead, (or populated and unpopulated, respectively). Every cell interacts with
-# its eight neighbours, which are the cells that are horizontally, vertically, or
-# diagonally adjacent. At each step in time, the following transitions occur:
-
-#     1. Any live cell with two or three live neighbors survives.
-#     2. Any dead cell with three live neighbors becomes a live cell.
-#     3. All other live cells die in the next generation. Similarly, all other dead cells stay dead.
-
-# The initial pattern constitutes the seed of the system. The first generation is
-# created by applying the above rules simultaneously to every cell in the seed;
-# births and deaths occur simultaneously, and the discrete moment at which this
-# happens is sometimes called a tick. Each generation is a pure function of the
-# preceding one. The rules continue to be applied repeatedly to create further generations.
-
-# for more read >>> https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 
 print('*' * 99)
 print("The Game of Life\n\n")
